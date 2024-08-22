ec2de62e5ceff4c08889ad1465b6080d2845e6d1
Jwill-81 <jhon.eto135@gmail.com>
2024-08-22 10:07:44 -0500

    hola mundo
    f

---
+++ git-estructura/src/git/estructura/GitEstructura.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git.estructura;
+public class GitEstructura {
+    public static void main(String[] args) {
+        System.out.println("puta");
+     
+    }
+    
+}
